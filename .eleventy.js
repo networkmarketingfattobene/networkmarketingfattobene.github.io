4cf912753302b8162792e48742a7db42a490d6b2
TEAMSYSTEM\a.pinato <a.pinato@teamsystem.com>
2025-07-29 10:32:34 +0200

    bozza template diviso in componenti

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 module.exports = function (eleventyConfig) {
+  
+  // Copia gli assets del template
+  eleventyConfig.addPassthroughCopy("src/assets");
+  
+  // Copia le librerie vendors
+  eleventyConfig.addPassthroughCopy("src/vendors");
+  
   return {
     dir: {
       input: "src",
